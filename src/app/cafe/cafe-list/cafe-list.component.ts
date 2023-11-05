756959687ff0936847be680562a6efb83565d47f
Francisco <f.roblesl@uniandes.edu.co>
2023-11-05 11:14:32 -0300

    Se adiciona modulo, clase y componente de cafe.

---
+++ src/app/cafe/cafe-list/cafe-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Cafe } from '../cafe';
+
+@Component({
+  selector: 'app-cafe-list',
+  templateUrl: './cafe-list.component.html',
+  styleUrls: ['./cafe-list.component.css']
+})
+export class CafeListComponent implements OnInit {
+
+  cafes: Array<Cafe> = []
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
