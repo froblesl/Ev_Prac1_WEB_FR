8d78ea760d112d31da65e25946313cff10b8e114
Francisco <f.roblesl@uniandes.edu.co>
2023-11-05 19:25:19 -0300

    Se adiciona punto 3, se incluye prueba.

--- src/app/cafe/cafe-list/cafe-list.component.spec.ts
+++ src/app/cafe/cafe-list/cafe-list.component.spec.ts
@@ -27,4 +27,35 @@ describe('CafeListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display 3 observations in the table', () => {
+    component.cafes = [
+      { id: 1, nombre: 'Café 1', tipo: 'Café de Origen', region: 'Región 1', sabor: "Canela", altura: 1900, imagen: "Imagen1" },
+      { id: 2, nombre: 'Café 2', tipo: 'Blend', region: 'Región 2', sabor: "Naranja", altura: 1800, imagen: "Imagen2" },
+      { id: 3, nombre: 'Café 3', tipo: 'Café de Origen', region: 'Región 3', sabor: "Manzana", altura: 1700, imagen: "Imagen3" },
+    ];
+    
+    fixture.detectChanges();
+
+    const table = fixture.nativeElement.querySelector('table');
+    expect(table).toBeTruthy();
+
+    const dataRows = table.querySelectorAll('tbody tr');
+    expect(dataRows.length).toBe(3);
+
+    const expectedData = [
+      ['1', 'Café 1', 'Café de Origen', 'Región 1'],
+      ['2', 'Café 2', 'Blend', 'Región 2'],
+      ['3', 'Café 3', 'Café de Origen', 'Región 3'],
+    ];
+
+    for (let i = 0; i < dataRows.length; i++) {
+        const cells = dataRows[i].querySelectorAll('td');
+        for (let j = 0; j < cells.length; j++) {
+            const cellText = cells[j].textContent.trim();
+            const expectedCellText = expectedData[i][j];
+            expect(cellText).toEqual(expectedCellText);
+        }
+    }
+  });
 });
